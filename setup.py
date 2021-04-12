612e036abc323eeff7cb5487b26cd2d4fd61b9bc
Aggelos Gkiokas <aggelos.gkiokas@upf.edu>
2021-04-12 19:03:50 +0200

    fixed setup.py updated requirements.txt

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='tpl',
+    name='tpc',
     version='1.0',
-    packages=['tpl'],
+    packages=['tpc'],
     url='https://github.com/trompamusic/tpc',
     license='Apache 2.0',
     author='aggelos',
     author_email='aggelos.gkiokas@upf.edu',
-    description='TROMPA Processing Library',
+    description='TROMPA Processing Component',
     install_requires=open("requirements.txt").read().split(),
 )
